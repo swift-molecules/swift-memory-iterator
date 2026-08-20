3bbf114a8193e8298cbf0b4a0e4e221c39f6ed8a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:07:37 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,25 +1,31 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-memory-iterator-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Memory Iterator Primitives",
             targets: ["Memory Iterator Primitives"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-span-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-iterator-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-span-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-iterator-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -34,7 +40,7 @@ let package = Package(
         .testTarget(
             name: "Memory Iterator Primitives Tests",
             dependencies: [
-                "Memory Iterator Primitives",
+                "Memory Iterator Primitives"
             ]
         ),
     ],
